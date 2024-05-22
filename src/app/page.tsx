e84e926c198e25ce964ec5c622fdd256d41316df
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-05-22 09:38:04 +0100

    tim corrections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,315 +48,239 @@ const socialMediaLinks = {
 export default async function Home() {
   return (
     <div>
-      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <ul role="list" className="divide-y divide-gray-200">
-          <li className="py-4">
-            <div className="flex gap-5">
-              <div className="w-[400px]">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <CardHorizontal imageUrl={testImage} />
-                </div>
-              </div>
-              <div className="flex">
-                <div>
-                  <h4 className="w text-lg font-bold text-red-600">
-                    Card Horizontal
-                  </h4>
-                  <p className="mt-1">
-                    All descriptions will be added here. If the title is red it
-                    means not finished
-                  </p>
-                </div>
-                <div className="">
-                  <CardProgramme
-                    imageUrl={testImage}
-                    programmeTitle="J-PAL"
-                    programmeType="abdul latif jameel poverty action lab"
-                  />
-                    <SectionUnderline children={undefined} />
-                </div>
-              </div>
-              <div className="flex">
-                <div>
-                  <h4 className="w costa-extra-bold text-lg font-bold text-red-600 ">
-                    Card programme
-                  </h4>
-                  <p className="mt-1">Now its just Image card</p>
-                </div>
+    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 py-9">
+      <h1 className={`text-4xl font-ibmMono`}>Basic icons</h1>
+      <ul role="list" className="">
+      <li className="py-9">
+          <div className="block">
+            <div className="w-[400px]">
+            <div>
+                <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
+                  Programme Card
+                </h2>
+                <p className="mb-6">The 'Programme Card' component has an aspect ratio of 1:1 and has a width that adadpts to the contraints of its parent wrapper. It </p>
               </div>
               <div className="">
-              </div>
-            </div>
-          </li>
-
-          <li className="py-9">
-            <div className="block">
-              <div className="w-[400px]">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <CardSquaredImage imageUrl={testImage} />
+                <CardProgramme
+                  imageUrl={testImage}
+                  programmeTitle="J-PAL"
+                  programmeType="abdul latif jameel poverty action lab"
+                />
                   <SectionUnderline children={undefined} />
-                </div>
-              </div>
-              <div className="flex">
-               
               </div>
             </div>
-          </li>
-          <li className="py-9">
-            <div className="block">
-              <div className="w-[400px]">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <SectionUnderline>
-                    <CardSquaredImage imageUrl={testImage} />
-                    <h1 className="text-3xl">Section Underline</h1>
-                  </SectionUnderline>
-                </div>
-              </div>
-              <div className="flex">
+            <div className="flex">
 
+            </div>
+          </div>
+        </li>
+        <li className="py-9">
+          <div className="block">
+            <div className="w-[400px]">
+            <div>
+            <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
+                  Card 16:9
+                </h2>
+                <p className="mb-6">
+                  The 'Card 16:9' component has an aspect ratio of 16:9 and has a width that adapts to the constraints of its parent wrapper. It also has an icon at the top right which can be changed dynamically based on the type of content it relates to.  
+                </p>
+              </div>
+              <div className="">
+                <CardHorizontal imageUrl={testImage} />
+                <SectionUnderline children={undefined} />
               </div>
             </div>
-          </li>
-          <li className="py-9">
-            <div className="flex gap-5">
-              <div className="w-[400px]">
-                <div className="">
-                  <SourceContainer
-                    source="New York Times"
-                    date={new Date(2024, 1, 16)}
-                  />
-                  <SourceContainer source="BBC News" date="16 February 2024" />
-                </div>
+            <div className="">
+            </div>
+          </div>
+        </li>
+
+        <li className="py-9">
+          <div className="block">
+            <div className="w-[400px]">
+            <div>
+              <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
+                  Card 1:1
+                </h2>
+                <p className="mb-6">The 'Card 1:1' component has an aspect ratio of 1:1 and has a width that adapts to the constraints of its parent wrapper. It also has an icon at the top right which can be changed dynamically based on the type of content it relates to.</p>
               </div>
-              <div className="flex">
-                <div>
-                  <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
-                    Card programme
-                  </h4>
-                  <p className="mb-6 ">Now its just Image card</p>
-                </div>
+              <div className="">
+                <CardSquaredImage imageUrl={testImage} />
+                <SectionUnderline children={undefined} />
               </div>
             </div>
-          </li>
-          <li className="py-9">
-            <div className="flex gap-5">
-              <div className="w-[400px]">
-                <div className="">
-                  <ButtonCJ>Click Me</ButtonCJ>
-                </div>
+            <div className="flex">
+             
+            </div>
+          </div>
+        </li>
+        <li className="py-9">
+          <div className="block">
+            <div className="w-[400px]">
+              <div>
+                <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
+                  Card 1:1 + text on right
+                </h2>
+                <Header4>Text</Header4>
               </div>
-              <div className="flex">
-                <div>
-                  <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
-                    Card programme
-                  </h4>
-                  <p className="mb-6 ">Now its just Image card</p>
-                </div>
+              <div className="">
+                <SectionUnderline>
+                  <CardSquaredImage imageUrl={testImage} />
+                  <h1 className="text-3xl">Section Underline</h1>
+                </SectionUnderline>
               </div>
             </div>
-          </li>
-          <li className="py-9">
-            <div className="flex gap-5">
-              <div className="w-[400px]">
-                <div className="">
-                  <Stats title="$21M" content="research grants" />
-                </div>
-              </div>
-              <div className="flex">
-                <div>
-                  <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
-                    Card programme
-                  </h4>
-                  <p className="mt-1 ">Now its just Image card</p>
-                </div>
+            <div className="flex">
+
+            </div>
+          </div>
+        </li>
+        <li className="py-9">
+          <div className="flex gap-5">
+            <div className="w-[400px]">
+              <div className="">
+                <SourceContainer
+                  source="New York Times"
+                  date={new Date(2024, 1, 16)}
+                />
+                <SourceContainer source="BBC News" date="16 February 2024" />
               </div>
             </div>
-          </li>
-          <li className="py-9">
             <div className="flex">
               <div>
-                <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
+                <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
                   Card programme
-                </h4>
+                </h2>
                 <p className="mb-6 ">Now its just Image card</p>
               </div>
             </div>
-            <div className=" gap-5">
-              <div className="w-full">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <TableCJ />
-                </div>
+          </div>
+        </li>
+        <li className="py-9">
+          <div className="flex gap-5">
+            <div className="w-[400px]">
+              <div className="">
+                <ButtonCJ>Click Me</ButtonCJ>
               </div>
             </div>
-          </li>
-          <li className="py-4">
             <div className="flex">
               <div>
-                <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
+                <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
                   Card programme
-                </h4>
-                <p className="mt-1 ">Now its just Image card</p>
+                </h2>
+                <p className="mb-6 ">Now its just Image card</p>
               </div>
             </div>
-            <div className=" gap-5">
-              <div className="w-full">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <ListContent
-                    title="Comparison of mammography AI algorithms with a clinical risk model for 5-year breast cancer risk prediction: An observational study"
-                    source="Nature"
-                    date="2023"
-                  />
-                </div>
+          </div>
+        </li>
+        <li className="py-9">
+          <div className="flex gap-5">
+            <div className="w-[400px]">
+              <div className="">
+                <Stats title="$21M" content="research grants" />
               </div>
             </div>
-          </li>
-          <li className="py-4">
             <div className="flex">
               <div>
-                <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
+                <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
                   Card programme
-                </h4>
-                <p className="mt-1 ">Now its just Image card</p>
+                </h2>
+                <p className="mb-6 ">Now its just Image card</p>
               </div>
             </div>
-            <div className=" gap-5">
-              <div className="w-full">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <ListSmall data={data} />
-                </div>
+          </div>
+        </li>
+        <li className="py-9">
+          <div className="flex">
+            <div>
+              <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
+                Card programme
+              </h2>
+              <p className="mb-6 ">Now its just Image card</p>
+            </div>
+          </div>
+          <div className="gap-5">
+            <div className="w-full">
+              <div className="">
+                <TableCJ />
               </div>
             </div>
-          </li>
-          <li className="py-4">
-            <div className="flex">
-              <div>
-                <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
-                  Card programme
-                </h4>
-                <p className="mt-1 ">Now its just Image card</p>
+          </div>
+        </li>
+        <li className="py-9">
+          <div className="flex">
+            <div className="w-[400px]">
+              <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
+                List item (text only)
+              </h2>
+              <p className="mb-6 ">The 'List item (text only)' component contains a 'title' and 'subtext' which can contain one or more pieces of dynamic data from the content type from which the list item's data is pulled.</p>
+            </div>
+          </div>
+          <div className=" gap-5">
+            <div className="w-[400px]">
+              <div className="">
+                <ListContent
+                  title="Comparison of mammography AI algorithms with a clinical risk model for 5-year breast cancer risk prediction: An observational study"
+                  source="Nature"
+                  date="2023"
+                />
               </div>
             </div>
-            <div className=" gap-5">
-              <div className="w-full">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <SocialMediaList {...socialMediaLinks} />
-                </div>
+          </div>
+        </li>
+        <li className="py-9">
+          <div className="flex">
+            <div>
+              <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
+                Card programme
+              </h2>
+              <p className="mb-6 ">Now its just Image card</p>
+            </div>
+          </div>
+          <div className=" gap-5">
+            <div className="w-full">
+              <div className="">
+                <ListSmall data={data} />
               </div>
             </div>
-          </li>
-          <li className="py-4">
-            <div className="flex">
-              <div>
-                <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
-                  Card programme
-                </h4>
-                <p className="mt-1 ">Now its just Image card</p>
+          </div>
+        </li>
+        <li className="py-9">
+          <div className="flex">
+            <div>
+              <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
+                Card programme
+              </h2>
+              <p className="mb-6 ">Now its just Image card</p>
+            </div>
+          </div>
+          <div className=" gap-5">
+            <div className="w-full">
+              <div className="">
+                <SocialMediaList {...socialMediaLinks} />
               </div>
             </div>
-            <div className=" gap-5">
-              <div className="w-full">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <TabsCJ />
-                </div>
+          </div>
+        </li>
+        <li className="py-9">
+          <div className="flex">
+            <div>
+              <h2 className={`text-2xl font-ibmMono text-red-600 mb-3 `}>
+                Card programme
+              </h2>
+              <p className="mb-6 ">Now its just Image card</p>
+            </div>
+          </div>
+          <div className=" gap-5">
+            <div className="w-full">
+              <div className="">
+                <TabsCJ />
               </div>
             </div>
-          </li>
-          <li className="py-4">
-            <div className="flex">
-              <div>
-                <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
-                  Card programme
-                </h4>
-                <p className="mt-1 ">Now its just Image card</p>
-              </div>
-            </div>
-            <div className=" gap-5">
-              <div className="w-full">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <Accordion
-                    panelContent={
-                      <div>
-                        <ol>
-                          <li>
-                            any type of component or html can be passed here
-                          </li>
-                        </ol>
-                        
-                      </div>
-                    }
-                    sentence={
-                      'Put your message here and pass any type of component '
-                    }
-                    bgColor="bg-blue-400"
-                  ></Accordion>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li className="py-4">
-            <div className="flex">
-              <div>
-                <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
-                  Card programme
-                </h4>
-                <p className="mt-1 ">Now its just Image card</p>
-              </div>
-            </div>
-            <div className=" gap-5">
-              <div className="w-full">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <Caroussel images={imageLinks} />
-                </div>
-              </div>
-            </div>
-          </li>
-          <li className="py-4">
-            <div className="flex">
-              <div>
-                <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
-                  Card programme
-                </h4>
-                <p className="mt-1 ">Now its just Image card</p>
-              </div>
-            </div>
-            <div className=" gap-5">
-              <div className="w-full">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                  <CarousselForComponents>
-                    <div className="h-48 w-48">
-                      <CardSquaredImage imageUrl={testImage} />
-                    </div>
-                    <div className="h-48 w-48">
-                      <CardSquaredImage imageUrl={testImage} />
-                    </div>
-                    <div className="h-48 w-48">
-                      <CardSquaredImage imageUrl={testImage} />
-                    </div>
-                 
-                  </CarousselForComponents>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li className="py-4">
-            <div className="flex">
-              <div>
-                <h4 className="w costa-extra-bold text-lg font-bold  text-red-600 ">
-                  Card programme
-                </h4>
-                <p className="mt-1 ">Now its just Image card</p>
-              </div>
-            </div>
-            <div className=" gap-5">
-              <div className="w-full">
-                <div className="w-full border-2 border-dotted border-gray-800 p-4 ">
-                <CarousselMIT />
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
+          </div>
+        </li>
+      </ul>
     </div>
+  </div>
   )
 }
